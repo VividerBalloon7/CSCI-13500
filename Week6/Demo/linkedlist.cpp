f77fc90a2df06279d5bcb01764617b62b006c1cf
Darius McLendon <darius.mclendon04@myhunter.cuny.edu>
2026-07-15 23:55:07 -0400

    :

--- Week6/Demo/linkedlist.cpp
+++ Week6/Demo/linkedlist.cpp
@@ -1,44 +1,88 @@
 #include <iostream>
+#include "linkedlist.hpp"
 
+// constructor: tells the computer how to build your custom data type
+Node::Node(int passed_value) {
+    // setting its own "value" variable to the passed_value
+    // 'this' is how an object refers to itself
+    this->value = passed_value;
+    this->next = nullptr;
+}
 
-struct Node{
+// constructor: initializes an empty linked list
+LinkedList::LinkedList() {
+    this->size_ = 0;
+    this->head_ = nullptr;
+}
 
-    std::string name = {};
-    Node* next_nodes_address = nullptr;
+void LinkedList::InsertNode(const int value) {
 
-    //constructor: tells the computer how to build your custom data type
-    Node(std::string user_data){
-        //setting its own "name" variable to the user_data
-        //'this' is how an object refers to itself
-        this -> name = user_data;
-    }
-};
+    // create a new node on the heap
+    Node* new_node = new Node(value);
 
-int main(){
-
-    Node head("start");
-    Node* current_node = &head; 
-    std::string user_input = {};
-
-    while (std::cin >> user_input){
-        if (user_input == "q"){
-            break;
-        }
-
-        current_node -> next_nodes_address = new Node(user_input);
-        current_node = current_node -> next_nodes_address;
+    // if the list is empty, the new node becomes the head
+    if (head_ == nullptr) {
+        head_ = new_node;
+        size_++;
+        return;
     }
 
-    current_node = &head; 
-    while (current_node){
-        std::cout << current_node->name << " -> ";
-        
-        
-        Node* next_node = current_node->next_nodes_address;
+    // start at the beginning of the list
+    Node* current_node = head_;
+
+    // move until we reach the final node
+    while (current_node->next != nullptr) {
+        current_node = current_node->next;
+    }
+
+    // connect the final node to the new node
+    current_node->next = new_node;
+
+    // increase the number of nodes in the list
+    size_++;
+}
+
+void LinkedList::PrintValues() const {
+
+    Node* current_node = head_;
+
+    // print every value in the list
+    while (current_node != nullptr) {
+        std::cout << current_node->value << " -> ";
+        current_node = current_node->next;
+    }
+
+    std::cout << "nullptr" << std::endl;
+}
+
+// destructor: frees all dynamically allocated nodes
+LinkedList::~LinkedList() {
+
+    Node* current_node = head_;
+
+    while (current_node != nullptr) {
+
+        Node* next_node = current_node->next;
+
         delete current_node;
-        current_node = nullptr;    // Prevent dangling pointers
+
         current_node = next_node;
     }
 
+    head_ = nullptr;
+    size_ = 0;
 }
 
+// temporary main function used to test the linked list
+int main() {
+
+    LinkedList list;
+
+    list.InsertNode(10);
+    list.InsertNode(20);
+    list.InsertNode(30);
+
+    list.PrintValues();
+
+    return 0;
+}
